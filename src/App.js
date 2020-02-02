f8a0e572fb230e7292ed48813f573a6e56ebb9f0
zhusain1 <zhusain1@umbc.edu>
2020-02-01 20:07:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+	withRouter
+} from 'react-router-dom';
+import './App.css'
+import Navbar from './Navbar';
+import logo from './logo.jpg';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+          bodyPart:"Chest",
+        };
+    
+        this.handleBodyPartChange = this.handleBodyPartChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleBodyPartChange(event){
+        this.setState({
+            bodyPart: event.target.value
+        });
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        const url = 'http://localhost:8080/workouts/type/'  + this.state.bodyPart;
+
+        axios.get(url)
+        .then(res => {
+            const workout = res.data;
+            this.props.history.push('/Workoutplan', { workout });
+        })
+      }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <div className="card  w-75">
+                    <h2> Workout Routine </h2>
+                    <img src={logo} alt="Logo" width="100" height="100" className="Image"/>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="bodyPart"> Body part to workout today: </label>
+                        <br/>
+                        <div className="col-3">
+                        <select className="form-control" value={this.state.bodyPart} onChange={this.handleBodyPartChange}>
+                            <option value="Chest">Chest</option>
+                            <option value="Arms">Arms</option>
+                            <option value="Legs">Legs</option>
+                            <option value="Back">Back</option>
+                            <option value="Abs">Abs</option>
+                            <option value="Cardio">Cardio</option>
+                        </select>
+                        <br/>
+                        </div>
+                        <p>
+                        <button type="submit" className="btn btn-primary">Generate Workout</button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
+export default withRouter(App);
---
+++ src/Create.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios, { post } from 'axios';
+import Navbar from './Navbar';
+
+class Create extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      files:null,
+      title:'',
+      type:'',
+      description:'',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeFile = this.handleChangeFile.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeType = this.handleChangeType.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this)
+  }
+
+  handleSubmit(event){
+    event.preventDefault() 
+    console.log("File submitted and data submitted");
+
+    // Save to DB
+    var backend = 'http://localhost:8080/workout';
+
+    var workoutData =  {
+      workoutName: this.state.title,
+      workoutDescription: this.state.description,
+      workoutType: this.state.type,
+      workoutUrl: this.state.files.name
+    };
+    axios({
+      method: 'post',
+      url: backend,
+      data: workoutData,
+      headers: {'Content-Type': 'application/json' }
+      })
+      .then(function (response) {
+          //handle success
+          console.log(response);
+      })
+      .catch(function (response) {
+          //handle error
+          console.log(response);
+      });
+
+      
+    // Save to S3
+    var server = 'http://localhost:8080/api/v1/workout-video/workoutVideo/upload';
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('file', this.state.files); 
+      
+    axios({
+      method: 'post',
+      url: server,
+      data: bodyFormData,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded' }
+      })
+      .then(function (response) {
+          //handle success
+          console.log(response);
+      })
+      .catch(function (response) {
+          //handle error
+          console.log(response);
+      });
+  }
+
+  handleChangeFile(event) {
+    let files = event.target.files;
+
+    console.log(files);
+    this.setState({ files: files[0] });
+  }
+
+  handleChangeTitle(event){
+    this.setState({ title: event.target.value });
+  }
+
+  handleChangeType(event){
+    this.setState({ type: event.target.value });
+  }
+
+  handleChangeDescription(event){
+    this.setState({description: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="Create">
+        <Navbar />
+        <div className="card w-75">
+                <h2> Create an exercise </h2>
+                <form onSubmit={this.handleSubmit}>
+                  <br/>
+                  <div className="col-5">
+                    <input type="text" className="form-control" placeholder="Title" 
+                    onChange={this.handleChangeTitle}/>
+                  </div>
+                  <br/>
+                  <div className="col-12">
+                    <input type="text" className="form-control" placeholder="Description" 
+                    onChange = {this.handleChangeDescription}/>
+                  </div>
+                  <br/>
+                  <div className="col-5"> 
+                    <select className="form-control" value={this.state.type} onChange={this.handleChangeType}>
+                      <option disabled value="">Bodypart to choose...</option>
+                      <option value="Chest">Chest</option>
+                      <option value="Arms">Arms</option>
+                      <option value="Legs">Legs</option>
+                      <option value="Back">Back</option>
+                      <option value="Abs">Abs</option>
+                      <option value="Cardio">Cardio</option>
+                    </select>
+                  </div>
+                  <br/>
+                  <div className="col-12">
+                    <small id="fileDescription" className="form-text text-muted">
+                      Choose a video that is less than 100 mb
+                    </small>
+                    <input type="file" onChange={this.handleChangeFile} className="form-control-file" />
+                  </div>
+                  <p>
+                    <button type="submit" className="btn btn-primary">Create</button>
+                  </p>
+                </form>
+           </div>             
+        </div>
+    );
+  }
+}
+
+
+export default Create;
