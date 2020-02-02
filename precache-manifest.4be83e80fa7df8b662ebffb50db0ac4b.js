2f05e9703e279237a967d6bce5d6ff6df1d0e621
zhusain1 <zhusain1@umbc.edu>
2020-02-01 21:27:55 -0500

    Updates

---
+++ precache-manifest.4be83e80fa7df8b662ebffb50db0ac4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b36f8b236e22bc4a602037d6843111",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c57bb448c9eaf198e54c",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "2bbe2368cc5a7f0b4718",
+    "url": "./static/css/main.3a7332c5.chunk.css"
+  },
+  {
+    "revision": "c57bb448c9eaf198e54c",
+    "url": "./static/js/2.99b8afd1.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "./static/js/2.99b8afd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bbe2368cc5a7f0b4718",
+    "url": "./static/js/main.d844ae41.chunk.js"
+  },
+  {
+    "revision": "9b8ccd7d2cc44ebb84c9",
+    "url": "./static/js/runtime-main.c333276d.js"
+  },
+  {
+    "revision": "9374f98afa67a42d92868901166786e1",
+    "url": "./static/media/logo.9374f98a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/workoutapp/precache-manifest.2b3b028cb61b0fc888086fa672d14342.js"
+  "./precache-manifest.4be83e80fa7df8b662ebffb50db0ac4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/workoutapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
