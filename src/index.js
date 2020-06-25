9385b8ebd0afda61fdd9047be8862ddef52ce41b
zhusain1 <zhusain1@umbc.edu>
2020-06-25 15:29:52 -0400

    disable logs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Nav from './Nav';
 import * as serviceWorker from './serviceWorker';
 
+function noop() {}
+console.log = noop;
+console.error = noop;
+console.warn = noop;
+
 ReactDOM.render(
     <Nav />, document.getElementById('root'));
 serviceWorker.unregister();
