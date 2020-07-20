11f7a76c0675e5b711e9fc21e85484da7b95e02a
zhusain1 <zhusain1@umbc.edu>
2020-07-20 17:25:55 -0400

    adding avatar

--- src/Navigationbar.js
+++ src/Navigationbar.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import account from './account.png';
 class Navigationbar extends Component {
     constructor(props){
         super(props)
@@ -12,6 +13,7 @@ class Navigationbar extends Component {
 
         this.state ={
             validRoute:false,
+            username: ""
         };
         
         /* Make call to check if code is valid from cookie */ 
@@ -32,9 +34,9 @@ class Navigationbar extends Component {
             })
             .then((response) => {
                 //handle success
-                console.log(response.data);
                 this.setState({
-                    validRoute: true
+                    validRoute: true,
+                    username: response.data
                 });
             })
             .catch((response) => {
@@ -49,30 +51,36 @@ class Navigationbar extends Component {
 
     render() {
         return (
-            <div className="NavBar">
-                <ul className="navItems">   
-                <Navbar expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <li>
-                            <Link to="/">Workouts</Link>
-                        </li>
-                        <li>
-                            {this.state.validRoute === true ?
-                            <Link to={{pathname:"/exerciseManager", state:{validRoute: this.state.validRoute}}}>
-                                Manage Exercises
-                            </Link>:
-                            <Link to="/login">Manage Exercises</Link>
-                            }
-                        </li>
-                        <li>
-                            <Link to="/createAccount">Create Account</Link>
-                        </li>
-                    </Nav>
-                </Navbar.Collapse>
-                </Navbar>
-                </ul>
+            <div>
+                <div className="NavBar">
+                    <ul className="navItems">   
+                    <Navbar expand="lg">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <li>
+                                <Link to="/">Workouts</Link>
+                            </li>
+                            <li>
+                                {this.state.validRoute === true ?
+                                <Link to={{pathname:"/exerciseManager", state:{validRoute: this.state.validRoute}}}>
+                                    Manage Exercises
+                                </Link>:
+                                <Link to="/login">Manage Exercises</Link>
+                                }
+                            </li>
+                            <li>
+                                <Link to="/createAccount">Create Account</Link>
+                            </li>
+                            <div className="user">
+                                <img src={account} alt="account" width="30" height="30"/> 
+                                <span className="avatar"> {this.state.username} </span>
+                            </div>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Navbar>
+                    </ul>
+                </div>
             </div>
         );
     }
