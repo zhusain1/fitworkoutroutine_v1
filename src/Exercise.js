f36b16812a5814d8bb2922d98514726a3c2d8bcd
zhusain1 <zhusain1@umbc.edu>
2020-08-13 13:31:19 -0400

    refacror of get videos

--- src/Exercise.js
+++ src/Exercise.js
@@ -4,22 +4,32 @@ import { Player } from 'video-react';
 import "../node_modules/video-react/dist/video-react.css"; // import css
 
 class Exercise extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            id: props.id,
+            title: props.title,
+            text: props.text,
+            type: props.type 
+        };
+    }
+    
     render() {
         return (
             <div>
-                <Accordion>
+                <Accordion defaultActiveKey="0">
                         <Card.Header>
                         <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            <h4> {this.props.title} </h4>
+                            <h4> {this.state.title} </h4>
                         </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
                         <Card.Body>
-                            {this.props.text}
+                            {this.state.text}
                             <br/>
                             <Player
                                 playsInline
-                                src={`https://workoutappapi.herokuapp.com/api/v1/workout-video/workoutVideo/download?workoutId=${this.props.id}`}
+                                src={`https://workoutappapi.herokuapp.com/api/v1/workout-video/workoutVideo/download?workoutId=${this.state.id}`}
                                 fluid={true}
                                 width={350}
                                 height={350}
