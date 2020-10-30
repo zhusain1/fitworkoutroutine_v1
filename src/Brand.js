72dfbfa1db98ffde47062411b7c6048fb214b1a6
zhusain1 <zhusain1@umbc.edu>
2020-10-30 15:14:20 -0400

    changing logo to come from css not img tag

--- src/Brand.js
+++ src/Brand.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css'
-import logo from './logo-transparent.png';
 
 class Brand extends Component{
     render(){
         return(
             <div className="Brand">
-                <img src={logo} alt="Logo" width="200" height="100" className="Image"/>
+                <div className="Image"/>
             </div>
         );
     }
