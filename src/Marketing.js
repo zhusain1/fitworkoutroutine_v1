8d78d9faa62fb0aaa5f60f4fc74b94fa2a7f676b
zhusain1 <zhusain1@umbc.edu>
2020-11-04 15:14:35 -0500

    Removing unused imports

--- src/Marketing.js
+++ src/Marketing.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PreNavigationbar from './PreNavigationbar';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Cookies from 'universal-cookie';
 import './App.css'
 
 class Marketing extends Component{
