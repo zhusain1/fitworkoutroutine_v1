c62155a1e4a18d72ea246051ee58f4d4e52640fe
zhusain1 <zhusain1@umbc.edu>
2020-11-06 13:42:02 -0500

    change navbar for error page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import {
 	withRouter
 } from 'react-router-dom';
-import Navigationbar from './Navigationbar';
+import PreNavigationbar from './PreNavigationbar';
 import { Link } from 'react-router-dom';
 
 class ErrorPage extends Component {
     render() {
         return (
             <div className="Errorpage">
-                <Navigationbar />
+                <PreNavigationbar />
                 <div className="card  w-75">
                     <h2> Looks like you went to a page that doesn't exist! </h2>
                    <p> Find your way back  <u ><Link to="/">home</Link> </u> </p>
